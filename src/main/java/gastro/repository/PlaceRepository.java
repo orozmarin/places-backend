99e42cb4dcbec5159fd8a64b664003d07ebd576b
bytemerin <marin.oroz@byteink.hr>
2024-05-20 15:18:48 +0200

    Refactoring everything from Restaurant to Place.

--- src/main/java/gastro/repository/PlaceRepository.java
+++ src/main/java/gastro/repository/PlaceRepository.java
@@ -1,14 +1,14 @@
 package gastro.repository;
 
-import gastro.model.Restaurant;
+import gastro.model.Place;
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RestaurantRepository extends MongoRepository<Restaurant, String> {
+public interface PlaceRepository extends MongoRepository<Place, String> {
     @Query("{name:'?0'}")
-    Restaurant findRestaurantByName(String name);
-    List<Restaurant> findByRestaurantRating(double rating);
+    Place findPlaceByName(String name);
+    List<Place> findByPlaceRating(double rating);
 }
---
+++ src/main/java/gastro/service/PlaceManager.java
@@ -0,0 +1,13 @@
+package gastro.service;
+
+import gastro.model.Place;
+import java.util.List;
+
+public interface PlaceManager {
+    Place saveOrUpdatePlace(Place place);
+    List<Place> findAllPlaces();
+    Place getPlaceByName(String name);
+    List<Place> getPlaceByRating(double rating);
+    String deletePlace(String placeId);
+
+}
