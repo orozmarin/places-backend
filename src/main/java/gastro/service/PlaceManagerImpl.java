c4668350be45a54bf8b590604167264afab7d143
bytemerin <marin.oroz@byteink.hr>
2024-05-20 15:18:48 +0200

    Refactoring everything from Restaurant to Place.

---
+++ src/main/java/gastro/service/PlaceManagerImpl.java
@@ -0,0 +1,46 @@
+package gastro.service;
+
+import gastro.model.Place;
+import gastro.repository.PlaceRepository;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlaceManagerImpl implements PlaceManager {
+
+    @Autowired
+    private PlaceRepository placeRepository;
+
+    @Override
+    public Place saveOrUpdatePlace(Place place) {
+        if (place.getId() == null) {
+            place.setId(UUID.randomUUID().toString().split("-")[0]);
+        }
+        place.setPlaceRating(
+                place.getFirstRating().getPlaceRating() + place.getSecondRating().getPlaceRating());
+        return placeRepository.save(place);
+    }
+
+    @Override
+    public List<Place> findAllPlaces() {
+        return placeRepository.findAll();
+    }
+
+    @Override
+    public Place getPlaceByName(String name) {
+        return placeRepository.findPlaceByName(name);
+    }
+
+    @Override
+    public List<Place> getPlaceByRating(double rating) {
+        return placeRepository.findByPlaceRating(rating);
+    }
+
+    @Override
+    public String deletePlace(String placeId) {
+        placeRepository.deleteById(placeId);
+        return "Place " + placeId + " deleted from database!";
+    }
+}
