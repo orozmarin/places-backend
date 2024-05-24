f8e7f912269b6fb3865b55895ca2169573963431
orozmarin <marinoroz42@gmail.com>
2024-05-24 14:17:07 +0200

    Added backend sorting for Place fetching via PlaceSearchForm. Added createdAt and visitedAt attributes.

--- src/main/java/places/repository/PlaceRepository.java
+++ src/main/java/places/repository/PlaceRepository.java
@@ -5,9 +5,10 @@ import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+import places.repository.qdls.PlaceRepositoryCustom;
 
 @Repository
-public interface PlaceRepository extends MongoRepository<Place, String> {
+public interface PlaceRepository extends MongoRepository<Place, String>, PlaceRepositoryCustom {
 
     @Query("{name:'?0'}")
     Place findPlaceByName(String name);
--- src/main/java/places/repository/qdls/PlaceRepositoryCustomImpl.java
+++ src/main/java/places/repository/qdls/PlaceRepositoryCustomImpl.java
@@ -1,23 +1,57 @@
 package places.repository.qdls;
 
-import jakarta.persistence.EntityManager;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.stereotype.Component;
 import places.model.Place;
 import places.model.PlaceSearchForm;
 
 @Slf4j
 @Component
-public class PlaceRepositoryCustomImpl implements PlaceRepositoryCustom{
-    private final EntityManager entityManager;
+public class PlaceRepositoryCustomImpl implements PlaceRepositoryCustom {
 
-    public PlaceRepositoryCustomImpl(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     @Override
     public List<Place> findPlacesBySearchForm(PlaceSearchForm placeSearchForm) {
-        return List.of();
+        List<Sort.Order> orders = new ArrayList<>();
+
+        switch (placeSearchForm.getSortingMethod()) {
+            case ALPHABETICALLY_ASC:
+                orders.add(Sort.Order.asc("name"));
+                break;
+            case ALPHABETICALLY_DESC:
+                orders.add(Sort.Order.desc("name"));
+                break;
+            case RATING_ASC:
+                orders.add(Sort.Order.asc("placeRating"));
+                break;
+            case RATING_DESC:
+                orders.add(Sort.Order.desc("placeRating"));
+                break;
+            case DATE_ASC:
+                orders.add(Sort.Order.asc("createdAt"));
+                break;
+            case DATE_DESC:
+                orders.add(Sort.Order.desc("createdAt"));
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid sorting method");
+        }
+
+        Aggregation aggregation = Aggregation.newAggregation(
+                Aggregation.sort(Sort.by(orders))
+        );
+
+        AggregationResults<Place> results = mongoTemplate.aggregate(aggregation, "places", Place.class);
+        return results.getMappedResults();
     }
 }
+
